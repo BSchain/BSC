b752331440cff75964137ffc70b3ddcb4a99e5c6
zfonemore <zfonemore@foxmail.com>
2018-01-23 12:27:02 +0800

    modify editInfo

--- BSCapp/static/app/javascript/editInfo.js
+++ BSCapp/static/app/javascript/editInfo.js
@@ -3,10 +3,28 @@ function EditInfo(){
     var btn = document.getElementById("edit");
     if (btn.innerHTML == "编辑")
     {
-        btn.innerHTML = "保存";
+        $("#edit").replaceWith("<div class='row'> <div class='col-md-6'> <button id='edit' class='btn btn-primary btn-lg btn-block' onclick='EditInfo()'>保存</button> </div> <div class='col-md-6'> <button id='cancel' class='btn btn-primary btn-lg btn-block' onclick='cancelEdit()'>取消</button> </div> </div>");
         var name = document.getElementById("nametext");
         var value = name.innerHTML;
-        name.innerHTML = "<input name='name' class='form-control' id='name' value="+value+">";
+        name.innerHTML = "<input name='name' id='name' value="+value+">";
+        var email = document.getElementById("emailtext");
+        var value = email.innerHTML;
+        email.innerHTML = "<input name='email' required type='email' id='email' value="+value+">";
+        var phone = document.getElementById("phonetext");
+        var value = phone.innerHTML;
+        phone.innerHTML = "<input name='phone' id='phone' value="+value+">";
+        var idcard = document.getElementById("idcardtext");
+        var value = idcard.innerHTML;
+        idcard.innerHTML = "<input name='idcard' id='idcard' value="+value+">";
+        var company = document.getElementById("companytext");
+        var value = company.innerHTML;
+        company.innerHTML = "<input name='company' id='company' value="+value+">";
+        var title = document.getElementById("titletext");
+        var value = title.innerHTML;
+        title.innerHTML = "<input name='title' id='title' value="+value+">";
+        var addr = document.getElementById("addrtext");
+        var value = addr.innerHTML;
+        addr.innerHTML = "<input name='addr' id='addr' value="+value+">";
     }
     else {
         $.ajax({
@@ -34,4 +52,10 @@ function EditInfo(){
           }
         })
     }
+}
+
+function cancelEdit()
+{
+    indexURL = "/userInfo/"
+    window.location.replace(indexURL);
 }
