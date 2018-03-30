42b1f9749f6185ad9fdfa2abb34b5e83c0d1d80e
zpfbuaa <zpflyfe@163.com>
2018-03-30 20:54:07 +0800

    add review data for admin

--- BSCapp/static/app/javascript/review.js
+++ BSCapp/static/app/javascript/review.js
@@ -25,3 +25,44 @@ function reviewReject(obj) {
       })
     return;
 }
+
+
+function hideAllPassData() {
+    $.ajax("/AdminDataInfo/", {
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            "func":'hideAllPass'
+        }
+      }).done(function(data){
+          if (data.statCode !=0){
+              alert(data.message)
+              return false
+          }
+          else{
+              alert(data.message)
+              window.location.replace("/AdminDataInfo/");
+          }
+      })
+    return;
+}
+
+function reviewDataAllPass() {
+    $.ajax("/AdminDataInfo/", {
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            "func":'reviewAllPass'
+        }
+      }).done(function(data){
+          if (data.statCode !=0){
+              alert(data.message)
+              return false
+          }
+          else{
+              alert(data.message)
+              window.location.replace("/AdminDataInfo/");
+          }
+      })
+    return;
+}
